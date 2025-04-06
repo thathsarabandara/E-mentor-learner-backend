5928c113c4ce9f6c8ca01732d1b5a88a9b215fc6
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-04-06 17:57:41 +0530

    modefied models

--- src/models/auth/User.model.ts
+++ src/models/auth/User.model.ts
@@ -10,6 +10,8 @@ const UserSchema: Schema = new Schema<IUser>({
     username: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String , required: true},
-});
+},
+    {timestamps: true}
+);
 
 export default mongoose.model("User", UserSchema)
