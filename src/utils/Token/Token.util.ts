91426a9cb68f2ca9204a673e4baab37afc1a694f
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-04-07 16:19:36 +0530

    add hash password utils

--- src/utils/Token/Token.util.ts
+++ src/utils/Token/Token.util.ts
@@ -2,6 +2,12 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const JWT_SECRET = process.env.JWT_SECRET as string
+
+if(!JWT_SECRET){
+    console.log("Enviornment variable for JWT_ SECRET not found!")
+}
+
 export const createToken = (email: string, password: string) =>{
-    return jwt.sign({email: email, password: password} , process.env.JWT_SECRET);
+    return jwt.sign({email: email, password: password}, JWT_SECRET);
 }
