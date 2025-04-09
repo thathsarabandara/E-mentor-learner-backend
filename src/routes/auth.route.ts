a0ebf0930651fbabeab9dbda8fdf2a252e4616ea
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-04-09 10:53:47 +0530

    completed the logic for password reset

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import { register }  from '../controllers/auth/register.controller';
 import { verifyOTP } from "../controllers/auth/verify.controller";
 import { resendOtp } from "../controllers/auth/otpResend.controller";
+import { passwordResetRequest, resetPassword, verifyResetToken } from "../controllers/auth/passReset.controller";
 
 const authRouter = Router();
 
 authRouter.post('/register', register);
 authRouter.post('/verifyotp', verifyOTP);
+authRouter.post('/resetrequest', passwordResetRequest);
+authRouter.post('/resetpassword', resetPassword);
+authRouter.get('/verifyresettoken', verifyResetToken);
 authRouter.get('/resendotp', resendOtp);
 
 export default authRouter;
